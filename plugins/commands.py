ffd29a99da1bbacdadda4ca987809271a0295e5d
soymadip <84225810+soymadip@users.noreply.github.com>
2022-01-14 11:40:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -41,9 +41,8 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Groups ➕', url=f'https://t.me/cinemazhub/13')
+            InlineKeyboardButton(' join Groups ', url=f'https://t.me/cinemazhub/13')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🤖 Updates', url='https://t.me/CineHub02')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
