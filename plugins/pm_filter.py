362b67bd58f74b41f995966bd0a06e7f919fd76b
soymadip <84225810+soymadip@users.noreply.github.com>
2022-01-18 13:59:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -61,7 +61,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}]\n{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"{file.file_name}\n[{get_size(file.file_size)}]", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -70,7 +70,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}]\n{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"{file.file_name}\n[{get_size(file.file_size)}]", callback_data=f'files#{file.file_id}'
                 ),
                 InlineKeyboardButton(
                     text=f"{get_size(file.file_size)}",
@@ -555,7 +555,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}]\n{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"{file.file_name}\n[{get_size(file.file_size)}]", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -564,7 +564,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}]\n{file.file_name}",
+                    text=f"{file.file_name}\n[{get_size(file.file_size)}]",
                     callback_data=f'files#{file.file_id}',
                 ),
                 InlineKeyboardButton(
