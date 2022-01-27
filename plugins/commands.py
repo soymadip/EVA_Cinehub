79b8daaf87cf2e306c6b442079b26eed7890f41e
soumadip <84225810+soymadip@users.noreply.github.com>
2022-01-27 23:56:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -10,6 +10,7 @@ from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
 from database.users_chats_db import db
 from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION
 from utils import get_size, is_subscribed, temp
+from telegraph import upload_file
 import re
 import json
 import base64
