2552dcc97386a9bc485e9cd50fdbd60b02fe78c7
soumadip <84225810+soymadip@users.noreply.github.com>
2022-01-28 09:26:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone https://github.com/soymadip/EVA_Cinehub.git /EvaMaria
-cd /EvaMaria
+git clone https://github.com/soymadip/EVA_Cinehub.git /EVA_Cinehub 
+cd /EVA_Cinehub 
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
