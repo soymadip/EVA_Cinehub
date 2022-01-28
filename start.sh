44097201450e6da708793b6a90e17ce29425a47c
soumadip <84225810+soymadip@users.noreply.github.com>
2022-01-28 09:17:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/soymadip/Eva-Cinehub.git /EvaMaria
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
-fi
-cd /EvaMaria
+echo "Cloning Repo, Please Wait..."
+git clone -b master https://github.com/soymadip/EVA_Cinehub.git /EVA_Cinehub
+cd /EVA_Cinehub
+echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Bot, Please Wait..."
 python3 bot.py
