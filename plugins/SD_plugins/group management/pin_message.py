959fc798197753ed36e7a42c9f0649764597b1dc
soymadip <84225810+soymadip@users.noreply.github.com>
2022-02-05 14:35:46 +0530

    pin add

---
+++ plugins/SD_plugins/group management/pin_message.py
@@ -0,0 +1,24 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from plugins.SD_plugins.cust_p_filters import (
+    admin_fliter
+)
+
+@Client.on_message(
+    filters.command(["pin"]) &
+    admin_fliter
+)
+async def pin(_, message: Message):
+    if not message.reply_to_message:
+        return
+    await message.reply_to_message.pin()
+
+
+@Client.on_message(
+    filters.command(["unpin"]) &
+    admin_fliter
+)
+async def unpin(_, message: Message):
+    if not message.reply_to_message:
+        return
+    await message.reply_to_message.unpin()
