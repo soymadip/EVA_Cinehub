f86dc1eae298d824fcf57f6abecdd3db4dab98a7
soumadip <84225810+soymadip@users.noreply.github.com>
2022-02-12 20:33:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,6 +53,8 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+CHANNEL = environ.get('CUSTOM_FOOTER')
+MDISK_TOKEN = environ.get('MDISK_TOKEN')
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
