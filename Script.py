629945dc6f216a3754e487348d590354a7d55d48
soumadip <84225810+soymadip@users.noreply.github.com>
2022-02-20 08:40:48 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -68,6 +68,34 @@ Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
 Added By - {}
 """
+    COMMANDS_TXT = """• /logs - to get the rescent errors
+• /stats - to get status of files in db.
+* /filter - add manual filters
+* /filters - view filters
+* /connect - connect to PM.
+* /disconnect - disconnect from PM
+* /del - delete a filter
+* /delall - delete all filters
+* /deleteall - delete all index(autofilter)
+* /delete - delete a specific file from index.
+* /info - get user info
+* /id - get tg ids.
+* /imdb - fetch info from imdb.
+• /users - to get list of my users and ids.
+• /chats - to get list of the my chats and ids 
+• /index  - to add files from a channel
+• /leave  - to leave from a chat.
+• /disable  -  do disable a chat.
+* /enable - re-enable chat.
+• /ban  - to ban a user.
+• /unban  - to unban a user.
+• /channel - to get list of total connected channels
+• /broadcast - to broadcast a message to all users.
+• /batch - to create link for multiple posts.
+• /links - to create link for one post.
+• /telegraph - upload media less than 5 MB to telegraph.
+• /share - get shareable link of any text or link.
+• /tmute <time> - temporarily mute user ."""
     LOG_TEXT_P = """🅽🅴🆆 🆄🆂🅴🆁
 🅘🅓 - <code>{}</code>
 ⓃⒶⓂ️Ⓔ - {}
