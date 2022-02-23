4b5655fb6e432b004a3947139dece860cf23f50f
soumadip <84225810+soymadip@users.noreply.github.com>
2022-02-23 20:51:52 +0530

    Sd

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -648,7 +648,7 @@ async def auto_filter(client, msg, spoll=False):
             logger.exception(e)
             fek = await message.reply_photo(photo="https://telegra.ph/file/473735000c944f51b38bd.jpg", caption=cap, reply_to_message_id=reply_id, reply_markup=InlineKeyboardMarkup(btn))
             await asyncio.sleep(240)
-            await fek.edit(f"\nTime Up, Your results are now deleted 🗑️")
+            await fek.edit(f"\n𝗧𝗶𝗺𝗲 𝗨𝗽\n⚙️ Result For A Wednesday Closed 🗑️")
             
     else:
         fuk = await message.reply_photo(photo="https://telegra.ph/file/473735000c944f51b38bd.jpg", caption=cap, reply_to_message_id=reply_id, reply_markup=InlineKeyboardMarkup(btn))
