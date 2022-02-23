0aefc12cf341f64f807059bd703ef113af6c7afe
soumadip <84225810+soymadip@users.noreply.github.com>
2022-02-24 00:01:18 +0530

    Sd

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -582,14 +582,14 @@ async def auto_filter(client, msg, spoll=False):
              InlineKeyboardButton(text="NEXT ⏩", callback_data=f"next_{req}_{key}_{offset}")]
         )
         btn.insert(0,
-            [InlineKeyboardButton(text="⭕️ 🇨 🇭 🇪 🇨 🇰  🇵 🇲 ⭕️",url="http://t.me/{temp.U_NAME}")]
+            [InlineKeyboardButton(text="⭕️ 🇨 🇭 🇪 🇨 🇰  🇵 🇲 ⭕️",url="http://t.me/{username}")]
         )
     else:
         btn.append(
             [InlineKeyboardButton(text="🗓 1/1", callback_data="pages")]
         )
         btn.insert(0,
-            [InlineKeyboardButton(text="⭕️ 🇨 🇭 🇪 🇨 🇰  🇵 🇲 ⭕️",url="http://t.me/{temp.U_NAME}")]
+            [InlineKeyboardButton(text="⭕️ 🇨 🇭 🇪 🇨 🇰  🇵 🇲 ⭕️",url="http://t.me/{username}")]
         )
     reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
