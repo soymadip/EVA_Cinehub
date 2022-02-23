c98beeb66e1be358bf71e91c5961eecd7310bb70
soumadip <84225810+soymadip@users.noreply.github.com>
2022-02-23 07:52:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -658,10 +658,10 @@ async def auto_filter(client, msg, spoll=False):
     else:
         fuk = await message.reply_photo(photo="https://telegra.ph/file/473735000c944f51b38bd.jpg", caption=cap, reply_to_message_id=reply_id, reply_markup=InlineKeyboardMarkup(btn))
         await asyncio.sleep(600)
-        await fuk.delete()
+        await fuk.edit(f"⚙️ Filter For {search} Closed 🗑️")
     if spoll:
         await msg.message.delete()
-
+        
 
 async def advantage_spell_chok(msg):
     query = re.sub(
