2c45ab9f00163667c127917b6c594bc1d03b2cbc
soumadip <84225810+soymadip@users.noreply.github.com>
2022-02-24 09:02:35 +0530

    Fix sticker/manual filter bug

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -543,38 +543,63 @@ if spoll:
 
 
 
-async def advantage_spell_chok(msg): query = re.sub( r"\b(pl(i|e) ?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?) |^h(e|a)?(l) (o) |mal(ayalam)?|t(h)?amil|file|that|find|und(o) |kit(t(i|y)?)?o(w)?|thar(u)?(o) w?|kittum(o) |aya(k) (um(o) )?|full\smovie|any(one)|with\ssubtitle(s)?)", "", msg.text, flags=re.IGNORECASE) # plis contribute some common words query = query.strip() + " movie" search = msg.text g_s = await search_gagala(query) g_s += await search_gagala(msg.text) gs_parsed = [] if not g_s: k = await msg.reply("if you are requesting movie, then waait until ADMIN responses.") await asyncio.sleep(8) await k.delete() return regex = re.compile(r". (imdb|wikipedia). ", re.IGNORECASE) # look for imdb / wiki results gs = list(filter(regex.match, g_s)) gs_parsed = [re.sub( r'\b(-([a-zA-Z-\s])-\simdb|(-\s)?imdb|(-\s)?wikipedia|(|)|-|reviews|full|all|episode(s)?|film|movie|series)', '', i, flags=re.IGNORECASE) for i in gs] if not gs_parsed: reg = re.compile(r"watch(\s[a-zA-Z0-9_\s-()] ) |. ", re.IGNORECASE)
-for mv in g_s: match = reg.match(mv) if match: gs_parsed.append(match.group(1)) user = msg.from_user.id if msg.from_user else 0 movielist = [] gs_parsed = list(dict.fromkeys(gs_parsed)) if len(gs_parsed) > 3: gs_parsed = gs_parsed[ 3] if gs_parsed for mov in gs_parsed: imdb_s = await get_poster(mov.strip(), bulk=True) # searching each keyword in imdb if imdb_s: hmm = InlineKeyboardMarkup( [ [ InlineKeyboardButton("️‍♂️ ️‍♂️", url=f" https://google.com/search?q={search} ") ] ] ) k = await msg.reply(f" .\nG - \n\n . ", reply_markup=hmm) return SPELL_CHECK[msg.message_id] = movielist btn = [[ InlineKeyboardButton( text=movie.strip(), callback_data=f"spolling#{user}#{k}", ) ] for k, movie in enumerate(movielist)] btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')]) m = await msg.reply(f"Hey, {msg.from_user.mention}!\nI couldn't find anything related to that\nDid you mean any one of these?", reply_markup=InlineKeyboardMarkup(btn)) await asyncio.sleep(10) await bot.send_message(LOG_CHANNEL, f" New Request - {search}\n From -{msg.from_user.mention}")
+async def manual_filters(client, message, text=False):
+    group_id = message.chat.id
+    name = text or message.text
+    reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
+    keywords = await get_filters(group_id)
+    for keyword in reversed(sorted(keywords, key=len)):
+        pattern = r"( |^|[^\w])" + re.escape(keyword) + r"( |$|[^\w])"
+        if re.search(pattern, name, flags=re.IGNORECASE):
+            reply_text, btn, alert, fileid = await find_filter(group_id, keyword)
 
-async def manual_filters(client, message, text=False): group_id = message.chat.id name = text or message.text reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id keywords = await get_filters(group_id) for keyword in reversed(sorted(keywords, key=len)): pattern = r"( |^|[^\w])" + re.escape(keyword) + r"( |$|[^\w])" if re.search(pattern, name, flags=re.IGNORECASE): reply_text, btn, alert, fileid = await find_filter(group_id, keyword)
+            if reply_text:
+                reply_text = reply_text.replace("\\n", "\n").replace("\\t", "\t")
 
-        if reply_text:
-            reply_text = reply_text.replace("\\n", "\n").replace("\\t", "\t")
-
-        if btn is not None:
-            try:
-                if fileid == "None":
-                    if btn == "[]":
-                       await message.reply_text(
-                         reply_text.format(
-                             first = message.from_user.first_name,
-                             last = message.from_user.last_name,
-                             fullname = message.from_user.first_name + " " + message.from_user.last_name,
-                             username = None if not message.from_user.username else '@' + message.from_user.username,
-                             mention = message.from_user.mention,
-                             id = message.from_user.id,
-                             dcid = message.from_user.dc_id,
-                             chatname = message.chat.title,
-                             query = name
-                         ),
-                         group_id,
-                         disable_web_page_preview=True,
-                         reply_to_message_id=reply_id
-                        )
-                    else:
-                        button = eval(btn)
-                        await message.reply_text(
-                            reply_text.format(
+            if btn is not None:
+                try:
+                    if fileid == "None":
+                        if btn == "[]":
+                            await client.send_message(
+                             reply_text.format(
+                                 first = message.from_user.first_name,
+                                 last = message.from_user.last_name,
+                                 fullname = message.from_user.first_name + " " + message.from_user.last_name,
+                                 username = None if not message.from_user.username else '@' + message.from_user.username,
+                                 mention = message.from_user.mention,
+                                 id = message.from_user.id,
+                                 dcid = message.from_user.dc_id,
+                                 chatname = message.chat.title,
+                                 query = name
+                             ),
+                             group_id,
+                             disable_web_page_preview=True,
+                             reply_to_message_id=reply_id
+                            )
+                        else:
+                            button = eval(btn)
+                            await client.send_message(
+                                reply_text.format(
+                                    first = message.from_user.first_name,
+                                    last = message.from_user.last_name,
+                                    fullname = message.from_user.first_name + " " + message.from_user.last_name,
+                                    username = None if not message.from_user.username else '@' + message.from_user.username,
+                                    mention = message.from_user.mention,
+                                    id = message.from_user.id,
+                                    dcid = message.from_user.dc_id,
+                                    chatname = message.chat.title,
+                                    query = name
+                                ),
+                                group_id,
+                                disable_web_page_preview=True,
+                                reply_markup=InlineKeyboardMarkup(button),
+                                reply_to_message_id = reply_id
+                            )
+                    elif btn == "[]":
+                        await client.send_cached_media(
+                            group_id,
+                            fileid,
+                            caption=reply_text.format(
                                 first = message.from_user.first_name,
                                 last = message.from_user.last_name,
                                 fullname = message.from_user.first_name + " " + message.from_user.last_name,
@@ -584,32 +609,31 @@ async def manual_filters(client, message, text=False): group_id = message.chat.i
                                 dcid = message.from_user.dc_id,
                                 chatname = message.chat.title,
                                 query = name
-                            ),
-                            group_id,
-                            disable_web_page_preview=True,
+                            ) or "",
+                            reply_to_message_id = reply_id
+                        )
+                    else:
+                        button = eval(btn) 
+                        await message.reply_cached_media(
+                            fileid,
+                            caption=reply_text.format(
+                                first=message.from_user.first_name,
+                                last=message.from_user.last_name,
+                                fullname = message.from_user.first_name + " " + message.from_user.last_name,
+                                username = None if not message.from_user.username else '@' + message.from_user.username,
+                                mention = message.from_user.mention,
+                                id=message.from_user.id,
+                                dcid = message.from_user.dc_id,
+                                chatname = message.chat.title,
+                                query = name
+                            ) or "",
                             reply_markup=InlineKeyboardMarkup(button),
                             reply_to_message_id = reply_id
                         )
-                elif btn == "[]":
-                    await client.send_cached_media(
-                        group_id,
-                        fileid,
-                        caption=reply_text or "",
-                        reply_to_message_id=reply_id
-                    )
-                else:
-                    button = eval(btn)
-                    await message.reply_cached_media(
-                        fileid,
-                        caption=reply_text or "",
-                        reply_markup=InlineKeyboardMarkup(button),
-                        reply_to_message_id=reply_id
-                    )
-            except Exception as e:
-                logger.exception(e)
-            break
-else:
-    return False
-
+                except Exception as e:
+                    logger.exception(e)
+                break
+    else:
+        return False
 
 
