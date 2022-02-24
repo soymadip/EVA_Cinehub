47afdc6139e4ba50476f62c272519b22d67cd2c3
soumadip <84225810+soymadip@users.noreply.github.com>
2022-02-24 08:41:35 +0530

    Fix!

--- info.py
+++ info.py
@@ -1,4 +1,5 @@
-import re from os import environ
+import re 
+from os import environ
 
 id_pattern = re.compile(r'^.\d+$') def is_enabled(value, default): if value.lower() in ["true", "yes", "1", "enable", "y"]: return True elif value.lower() in ["false", "no", "0", "disable", "n"]: return False else: return default
 
