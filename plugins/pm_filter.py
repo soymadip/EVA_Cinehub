cffd6e90b46fa8c647dafb6c38324f268e9c95e2
soumadip <84225810+soymadip@users.noreply.github.com>
2022-02-25 00:48:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -425,7 +425,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         [
         InlineKeyboardButton('𝔸𝕓𝕠𝕦𝕥', callback_data='about'),
         InlineKeyboardButton('𝕊𝕠𝕦𝕣𝕔𝕖', callback_data='source')
-        ],[InlineKeyboardButton('ℹ️ ℂ𝕠𝕞𝕞𝕒𝕟𝕕𝕤 ℹ️', callback_data='source')] 
+        ],[InlineKeyboardButton('ℹ️ ℂ𝕠𝕞𝕞𝕒𝕟𝕕𝕤 ℹ️', url='https://telegra.ph/All-Commands-For-Eva-Cinehub-02-24')] 
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
