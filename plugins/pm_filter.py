f1f3dd3c55ef885e4685eccb222a57a3b392a439
soymadip <84225810+soymadip@users.noreply.github.com>
2022-02-24 20:20:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1009,7 +1009,7 @@ async def advantage_spell_chok(msg):
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
     m = await msg.reply(f"i think you wrote wrong spelling\n\n<b>Did you mean any one of these</b>👇?", reply_markup=InlineKeyboardMarkup(btn))
     await asyncio.sleep(20)
-    await m.edit(f"you didn't respond🤷🏼")
+    await m.delete
 
 
 async def manual_filters(client, message, text=False):
