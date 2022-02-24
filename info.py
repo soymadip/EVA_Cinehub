45b5baf9b12f56e514517c3a893d38919a4a4fc7
soumadip <84225810+soymadip@users.noreply.github.com>
2022-02-24 10:09:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,16 +4,11 @@ from os import environ
 id_pattern = re.compile(r'^.\d+$')
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
-                return True
-                                elif value.lower() in ["false", "no", "0", "disable", "n"]:
-                                                        return False
-                                                                                    else:
-                                                                                                                        return default
-                                                                                                                        # Bot information
-                                                                                                                        SESSION = environ.get('SESSION', 'Media_search')
-                                                                                                                        API_ID = int(environ['API_ID'])
-                                                                                                                        API_HASH = environ['API_HASH']
-                                                                                                                        BOT_TOKEN = environ['BOT_TOKEN']
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default                                                                                                                        BOT_TOKEN = environ['BOT_TOKEN']
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
