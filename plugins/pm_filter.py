09f74d7018fbc7f3f2f965f15989d672dc72fb0f
soymadip <84225810+soymadip@users.noreply.github.com>
2022-02-27 17:17:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -37,22 +37,6 @@ async def give_filter(client, message):
         await auto_filter(client, message)
 
 
-
-@Client.on_message(filters.text & filters.private & filters.incoming) #PM guide module
-async def filter(client, message):
-    if message.text.startswith("/"):
-        return 
-    if 2 < len(message.text) < 10:
-        btn = [
-        [
-            InlineKeyboardButton('⚡️ ℂ𝕀ℕ𝔼𝕄𝔸 ℍ𝕌𝔹 ⚡️', url=f'https://t.me/cinemaforyou07')
-        ]
-        ]
-        await client.send_message(chat_id=message.from_user.id, text='🔰𝗡𝗢𝗧𝗜𝗖𝗘🔰\n\nDo not request here😡\nThis chat is only for <u>movie delevery</u>.\n\n<b>Request in CINEMA HUB group👇🏻</b>', reply_markup=InlineKeyboardMarkup(btn))
-
-
-
-
 @Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
