bc6fe9baf06e237484ab74e4466c589ae31eb485
soymadip <84225810+soymadip@users.noreply.github.com>
2022-02-27 17:23:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -813,6 +813,20 @@ async def advantage_spell_chok(msg):
     await asyncio.sleep(100)
     await m.delete()
 
+
+@Client.on_message(filters.text & filters.private & filters.incoming) #PM guide module
+async def filter(client, message):
+    if message.text.startswith("/"):
+        return 
+    if 2 < len(message.text) < 10:
+        btn = [
+        [
+            InlineKeyboardButton('⚡️ ℂ𝕀ℕ𝔼𝕄𝔸 ℍ𝕌𝔹 ⚡️', url=f'https://t.me/cinemaforyou07')
+        ]
+        ]
+        await client.send_message(chat_id=message.from_user.id, text='🔰𝗡𝗢𝗧𝗜𝗖𝗘🔰\n\nDo not request here😡\nThis chat is only for <u>movie delevery</u>.\n\n<b>Request in CINEMA HUB group👇🏻</b>', reply_markup=InlineKeyboardMarkup(btn))
+
+
 async def manual_filters(client, message, text=False):
     group_id = message.chat.id
     name = text or message.text
