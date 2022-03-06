5d409c142b0a1893117d58f6b27dca982cbf68f5
soymadip <84225810+soymadip@users.noreply.github.com>
2022-03-06 15:49:14 +0530

    sd

--- info.py
+++ info.py
@@ -54,6 +54,7 @@ MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 MELCOW_NEW_TEXT = environ.get("MELCOW_NEW_TEXT", "Don't forget to suscribe channel.")
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+MAINCHANNEL_ID = os.environ.get("MAINCHANNEL_ID", "-1001510870866")
 CHANNEL = environ.get('CUSTOM_FOOTER')
 
 
