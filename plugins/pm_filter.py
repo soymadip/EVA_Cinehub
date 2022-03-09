e8da00907749f42e722e36976a9d4bc4d022491f
soymadip <84225810+soymadip@users.noreply.github.com>
2022-03-09 11:52:04 +0530

    :100:

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -145,7 +145,7 @@ async def advantage_spoll_choker(bot, query):
             await auto_filter(bot, query, k)
         else:
             await query.message.edit(f'🧑🏽‍💻 I think it is not uploaded. Wait until admin uploads.')
-            await bot.send_message(LOG_CHANNEL,f'Pending Request of {query.from_user.first_name}\n{query.message}\n\n<b>Link</b> : {SUPPORT_CHAT}/{query.message.reply_to_message.message_id}', disable_web_page_preview= True)
+            await bot.send_message(LOG_CHANNEL,f'New Request of {query.from_user.first_name}\n\n<b> × Link</b> : {SUPPORT_CHAT}/{query.message.reply_to_message.message_id}', disable_web_page_preview= True)
 
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
