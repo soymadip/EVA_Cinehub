baa9bf9abe90efd29c3d67d96c7f464ada4864cb
soumadip <84225810+soymadip@users.noreply.github.com>
2022-03-09 15:56:06 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -259,10 +259,10 @@ async def list_chats(bot, message):
 async def filter(client, message):
     if message.text.startswith("/"):
         return 
-    if 2 < len(message.text) < 20:
+    if 2 < len(message.text) < 30:
         btn = [
         [
             InlineKeyboardButton('⚡️ ℂ𝕀ℕ𝔼𝕄𝔸 ℍ𝕌𝔹 ⚡️', url=f'https://t.me/cinemaforyou07')
         ]
         ]
-        await client.send_message(chat_id=message.from_user.id, text='🔰𝗡𝗢𝗧𝗜𝗖𝗘🔰\n\nDo not request here😡\nThis chat is only for <u>movie delevery</u>.\n\n<b>Request in CINEMA HUB group👇🏻</b>', reply_markup=InlineKeyboardMarkup(btn))
+        await client.send_message(chat_id=message.from_user.id, text='🔰𝗡𝗢𝗧𝗜𝗖𝗘🔰\n\nDo not request here😡\nThis chat is only for <u>movie delevery</u>.\n\n<b>Tell in CINEMA HUB group👇🏻</b>', reply_markup=InlineKeyboardMarkup(btn))
