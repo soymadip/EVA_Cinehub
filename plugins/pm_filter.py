580554a5175116f61403075f8cee480bc36a5eb2
soymadip <84225810+soymadip@users.noreply.github.com>
2022-03-09 12:09:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -145,10 +145,7 @@ async def advantage_spoll_choker(bot, query):
             await auto_filter(bot, query, k)
         else:
             await query.message.edit(f'🧑🏽‍💻 I think it is not uploaded. Wait until admin uploads.')
-            await bot.send_message(LOG_CHANNEL,f'New Request of {query.from_user.first_name}\n\n<b>✘Link</b> : {SUPPORT_CHAT}/{query.message.reply_to_message.message_id}', disable_web_page_preview= True)
-            await asyncio.sleep(15)
-            await bot.send_message(chat_id=message.from_user.id, text='🧑🏽‍💻 I think it is not uploaded. Wait until admin uploads.')
-
+            await bot.send_message(LOG_CHANNEL,f'New Request of {query.from_user.first_name}\n\n<b>✘Link</b> :- {SUPPORT_CHAT}/{query.message.reply_to_message.message_id}', disable_web_page_preview= True)
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
     if query.data == "close_data":
