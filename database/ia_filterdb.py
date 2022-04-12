4937986a525d8663e3bc5ad9c6c326b6acca7090
soumadip <soumadip377@gmail.com>
2022-04-12 09:45:03 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -3,6 +3,7 @@ from struct import pack
 import re
 import base64
 from pyrogram.file_id import FileId
+from pyrogram import Client
 from pymongo.errors import DuplicateKeyError
 from umongo import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
