db9cf58e2386ac591138d7345d819eb2b9f21f48
soumadip <soumadip377@gmail.com>
2022-04-15 08:37:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -584,7 +584,8 @@ async def auto_filter(client, msg, spoll=False):
         message = msg
         settings = await get_settings(message.chat.id)
         if message.text.startswith("/"): return  # ignore commands
-        if message.text.startswith("#"): return
+        if message.text.startswith("#"): return  # ignore wrong formats
+        if message.text.startswith("."): return. # ignore userbot commands
         if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
             return
         if 1 < len(message.text) < 100:
