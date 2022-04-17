acae5e486c0e01fda60abf9bc3245b4a3fda1127
soymadip <84225810+soymadip@users.noreply.github.com>
2022-04-17 21:57:02 +0530

    logic bro logic !

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -277,6 +277,8 @@ async def gen_invite(bot, message):
 @Client.on_message(filters.text & filters.private & ~filters.edited & filters.incoming) #PM guide module
 async def filter(client, message):
     if PM_FILTER:
+        return
+    else:
         if AUTH_USERS and message.from_user and message.from_user.id in AUTH_USERS:
             return True
         if message.text.startswith("/"):
@@ -291,6 +293,5 @@ async def filter(client, message):
                 await message.reply_text(f"🔰𝗡𝗢𝗧𝗜𝗖𝗘🔰\n\nService is 𝕔𝕝𝕠𝕤𝕖𝕕 for 𝟮 𝘄𝗲𝗲𝗸𝘀.\nwill start again by <u>next month.</u>.\n\n𝖡𝗒 𝗍𝗁𝗂𝗌 𝗍𝗂𝗆𝖾, 𝖬𝖺𝗄𝖾 𝗌𝗎𝗋𝖾 <b>you have 𝗌𝗎𝖻𝗌𝖼𝗋𝗂𝖻𝖾𝖽 CINEMA HUB group👇🏻</b>", reply_markup=InlineKeyboardMarkup(btn))
             else:
                 await client.send_message(chat_id=message.from_user.id, text='🔰𝗡𝗢𝗧𝗜𝗖𝗘🔰\n\nDo not request here😡\nThis chat is only for <u>movie delevery</u>.\n\n<b>Tell your query in CINEMA HUB group👇🏻</b>', reply_markup=InlineKeyboardMarkup(btn))
-    else:
-        return            
+           
 
