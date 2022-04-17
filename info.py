42aca5a1f4f074e5051c4e431c306bdfe794af03
soumadip <soumadip377@gmail.com>
2022-04-17 19:41:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,6 +30,7 @@ auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
+MAINTENANCE_MODE = environ.get('MAINTENANCE_MODE', 'False')
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
