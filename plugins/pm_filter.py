84c29c71a9697948b9cc844b086202c03d412cb5
soymadip <84225810+soymadip@users.noreply.github.com>
2022-04-17 23:43:52 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -50,13 +50,6 @@ async def give_filter(client, message):
             await auto_filter(client, message)
 
 
-@Client.on_message(filters.text & ~filters.edited & filters.incoming)
-async def give_filter(client, message):
-    k = await manual_filters(client, message)
-    if k == False:
-        await auto_filter(client, message)
-
-
 @Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
