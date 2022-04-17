4106c605ec2a32a161fde371690dd0ad6fc172c2
soymadip <84225810+soymadip@users.noreply.github.com>
2022-04-17 20:45:39 +0530

    kuio8o

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-MAINTENANCE_MODE = environ.get('MAINTENANCE_MODE', 'False')
+MAINTENANCE_MODE = is_enabled((environ.get('MAINTENANCE_MODE', "False")), False)
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
