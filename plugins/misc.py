1dd6b56c1cff58166b9d281f446f91e0e7928703
soymadip <84225810+soymadip@users.noreply.github.com>
2022-04-30 16:18:28 +0530

    sdsdsdsd

--- plugins/misc.py
+++ plugins/misc.py
@@ -56,12 +56,9 @@ async def showid(client, message):
 
 @Client.on_message(filters.command(["info"]))
 async def who_is(client, message):
-    # https://github.com/SpEcHiDe/PyroGramBot/blob/master/pyrobot/plugins/admemes/whois.py#L19
+    """ extract user information """
     status_message = await message.reply_text(
-        "`Fetching user info...`"
-    )
-    await status_message.edit(
-        "`Processing user info...`"
+        "Wait Bro Let Me Check 🙂"
     )
     from_user = None
     from_user_id, _ = extract_user(message)
@@ -71,25 +68,35 @@ async def who_is(client, message):
         await status_message.edit(str(error))
         return
     if from_user is None:
-        return await status_message.edit("no valid user_id / message specified")
-    message_out_str = ""
-    message_out_str += f"<b>✘First Name:</b> {from_user.first_name}\n"
-    last_name = from_user.last_name or "<b>None</b>"
-    message_out_str += f"<b>✘Last Name:</b> {last_name}\n"
-    message_out_str += f"<b>✘Telegram ID:</b> <code>{from_user.id}</code>\n"
-    username = from_user.username or "<b>None</b>"
-    dc_id = from_user.dc_id or "[Not a Valid Dc]"
-    message_out_str += f"<b>✘Data Centre:</b> <code>{dc_id}</code>\n"
-    message_out_str += f"<b>✘User Name:</b> @{username}\n"
-    message_out_str += f"<b>✘User 𝖫𝗂𝗇𝗄:</b> <a href='tg://user?id={from_user.id}'><b>Click Here</b></a>\n"
-    if message.chat.type in (("supergroup", "channel")):
+        await status_message.edit("no valid user_id / message specified")
+        return
+    
+    first_name = from_user.first_name or ""
+    last_name = from_user.last_name or ""
+    username = from_user.username or ""
+    
+    message_out_str = (
+        "<b>᚛› 𝙽𝙰𝙼𝙴 :</b> "
+        f"<a href='tg://user?id={from_user.id}'>{first_name}</a>\n"
+        f"<b>᚛› 𝚂𝚄𝙵𝙵𝙸𝚇 :</b> {last_name}\n"
+        f"<b>᚛› 𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴 :</b> @{username}\n"
+        f"<b>᚛› 𝚄𝚂𝙴𝚁 𝙸𝙳 :</b> <code>{from_user.id}</code>\n"
+        f"<b>᚛› 𝚄𝚂𝙴𝚁 𝙻𝙸𝙽𝙺 :</b> {from_user.mention}\n" if from_user.username else ""
+        f"<b>᚛› 𝙸𝚂 𝙰𝙲𝙲𝙾𝚄𝙽𝚃 𝙳𝙴𝙻𝙴𝚃𝙴𝙳 :</b> True\n" if from_user.is_deleted else ""
+        f"<b>᚛› 𝙸𝚂 𝚅𝙴𝚁𝙸𝙵𝙸𝙴𝙳 :</b> True" if from_user.is_verified else ""
+        f"<b>᚛› 𝙸𝚂 𝚂𝙲𝙰𝙼 :</b> True" if from_user.is_scam else ""
+        # f"<b>Is Fake:</b> True" if from_user.is_fake else ""
+        f"<b>᚛› 𝙻𝙰𝚂𝚃 𝚂𝙴𝙴𝙽 :</b> <code>{last_online(from_user)}</code>\n\n"
+    )
+
+    if message.chat.type in ["supergroup", "channel"]:
         try:
             chat_member_p = await message.chat.get_member(from_user.id)
             joined_date = datetime.fromtimestamp(
                 chat_member_p.joined_date or time.time()
             ).strftime("%Y.%m.%d %H:%M:%S")
             message_out_str += (
-                "<b>✘Joined this Chat on:</b> <code>"
+                "<b>Joined on:</b> <code>"
                 f"{joined_date}"
                 "</code>\n"
             )
@@ -100,29 +107,17 @@ async def who_is(client, message):
         local_user_photo = await client.download_media(
             message=chat_photo.big_file_id
         )
-        buttons = [[
-            InlineKeyboardButton('✘ Close ✘', callback_data='close_data')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=local_user_photo,
             quote=True,
-            reply_markup=reply_markup,
             caption=message_out_str,
-            parse_mode="html",
             disable_notification=True
         )
         os.remove(local_user_photo)
     else:
-        buttons = [[
-            InlineKeyboardButton('✘ Close ✘', callback_data='close_data')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_text(
             text=message_out_str,
-            reply_markup=reply_markup,
             quote=True,
-            parse_mode="html",
             disable_notification=True
         )
     await status_message.delete()
