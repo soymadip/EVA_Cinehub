2cfcf405f649cdc2a63e6769507094a70ce21d10
soumadip <soumadip377@gmail.com>
2022-05-07 20:45:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,9 +33,14 @@ class Bot(Client):
         temp.BANNED_CHATS = b_chats
         await super().start()
         await Media.ensure_indexes()
+        btn = [
+        [
+            InlineKeyboardButton('⚡️ ℂ𝕀ℕ𝔼𝕄𝔸 ℍ𝕌𝔹 ⚡️', url=f'https://t.me/cinemaforyou07')
+        ]
+        ]
         await self.send_message(
             chat_id=-1001308633613,
-            text="🧭🧭 <b>GROUP OPENED</b> 🧭🧭\n\n✅ Requests are allowed. Let's start today's works.\n\n🌄 Good morning."
+            text="🧭🧭 <b>GROUP OPENED</b> 🧭🧭\n\n✅ Requests are allowed. Let's start today's works.\n\n🌄 Good morning.", reply_markup=InlineKeyboardMarkup(btn)
         )
         me = await self.get_me()
         temp.ME = me.id
