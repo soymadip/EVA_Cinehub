1dd0ce7f4064bea163e0ac84970b0a767ecce0d6
soymadip <84225810+soymadip@users.noreply.github.com>
2022-05-25 16:39:29 +0530

    complete failure :-1:

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -4,28 +4,15 @@ import datetime
 import time
 from database.users_chats_db import db
 from info import ADMINS
-from utils import qbroadcast_messages, wqbroadcast_messages
+from utils import wqbroadcast_messages
 import asyncio
         
-@Client.on_message(filters.command("broadcasts") & filters.user(ADMINS) & filters.reply)
-async def speaker(bot, message):
-    btn = [
-        [
-            InlineKeyboardButton('⚡️ Without Quote ⚡️', callback_data="wqbroadcast")
-        ],
-        [
-            InlineKeyboardButton('⚡️ With Quote ⚡️', callback_data="qbroadcast")
-        ]
-        ]
-    await message.reply(f"which mode do you preffer?\nFor refferance go to <a href= http://www.github.com/soymadip/Eva_Cinehub>File name:</a>", reply_markup=InlineKeyboardMarkup(btn))
-
-
-@Client.on_callback_query(filters.regex(r'^wqbroadcast'))
+@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
 async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     sts = await message.reply_text(
-        text='Broadcasting your messages without quote...'
+        text='Broadcasting your messages...'
     )
     start_time = time.time()
     total_users = await db.total_users_count()
@@ -48,43 +35,7 @@ async def verupikkals(bot, message):
                 failed += 1
         done += 1
         await asyncio.sleep(2)
-        if not done % 10:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
-
-
-
-
-@Client.on_callback_query(filters.regex(r'^qbroadcast'))
-async def verupikkals(bot, message):
-    users = await db.get_all_users()
-    b_msg = message.reply_to_message
-    sts = await message.reply_text(
-        text='Broadcasting your messages with quote...'
-    )
-    start_time = time.time()
-    total_users = await db.total_users_count()
-    done = 0
-    blocked = 0
-    deleted = 0
-    failed =0
-
-    success = 0
-    async for user in users:
-        pti, sh = await qbroadcast_messages(int(user['id']), b_msg)
-        if pti:
-            success += 1
-        elif pti == False:
-            if sh == "Blocked":
-                blocked+=1
-            elif sh == "Deleted":
-                deleted += 1
-            elif sh == "Error":
-                failed += 1
-        done += 1
-        await asyncio.sleep(2)
-        if not done % 10:
+        if not done % 20:
             await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
