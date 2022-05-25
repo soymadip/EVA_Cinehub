a6645d611ab31f9631d76d780c6d167ae622cd3b
soymadip <84225810+soymadip@users.noreply.github.com>
2022-05-25 16:34:25 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -17,7 +17,7 @@ async def speaker(bot, message):
             InlineKeyboardButton('⚡️ With Quote ⚡️', callback_data="qbroadcast")
         ]
         ]
-    await messagemsg.reply(f"which mode do you preffer?\nFor refferance go to <a href= http://www.github.com/soymadip/Eva_Cinehub>File name:</a>", reply_markup=InlineKeyboardMarkup(btn))
+    await message.reply(f"which mode do you preffer?\nFor refferance go to <a href= http://www.github.com/soymadip/Eva_Cinehub>File name:</a>", reply_markup=InlineKeyboardMarkup(btn))
 
 
 @Client.on_callback_query(filters.regex(r'^wqbroadcast'))
