a5d2c99f58dd255f22446a7f102d6e11daf5136a
soumadip <soumadip377@gmail.com>
2022-05-28 22:00:14 +0530

    Update json.py

--- plugins/SD_plugins/json.py
+++ plugins/SD_plugins/json.py
@@ -1,8 +1,6 @@
 import os
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
-from user import USER
-from info import LOG_CHANNEL
 
 @Client.on_message(filters.command(["json", 'js', 'showjson']))
 async def jsonify(bot, message):
@@ -25,7 +23,6 @@ async def jsonify(bot, message):
             ]
         )
         await message.reply_text(f"{the_real_message}", reply_markup=pk, quote=True)
-        await bot.USER.send_message(LOG_CHANNEL, f"hcufuvuvuvvhvuvuv")
     except Exception as e:
         with open("json.text", "w+", encoding="utf8") as out_file:
             out_file.write(str(the_real_message))
