756eb209bde11c3edc56b350419f00de3b468609
soumadip <soumadip377@gmail.com>
2022-05-28 21:12:34 +0530

    Create user.py

---
+++ user.py
@@ -0,0 +1,25 @@
+
+
+from pyrogram import (
+    Client,
+    __version__
+)
+
+from info import (
+    API_HASH,
+    API_ID,
+    LOGGER,
+    TG_BOT_WORKERS,
+    TG_USER_SESSION
+)
+
+class User(Client):
+    def __init__(self):
+        super().__init__(
+            TG_USER_SESSION,
+            api_hash=API_HASH,
+            api_id=API_ID,
+            workers=4
+        )
+ 
+ 
