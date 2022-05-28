55b6e42e8bd97566489bf27ee51aa05b5fa12e3c
soumadip <soumadip377@gmail.com>
2022-05-28 22:34:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,10 @@ from database.ia_filterdb import Media
 from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
 from utils import temp
+
 from user import User
+
+
 
 class Bot(Client):
     USER: User = None
@@ -23,45 +26,29 @@ class Bot(Client):
     def __init__(self):
         super().__init__(
             SESSION,
-            api_id=API_ID,
             api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            workers=50,
-            plugins={"root": "plugins"},
-            sleep_threshold=5,
+            api_id=API_ID,
+            plugins={
+                "root": "plugins"
+            },
+            workers=TG_BOT_WORKERS,
+            bot_token=TG_BOT_TOKEN
         )
 
     async def start(self):
+        await super().start()
         b_users, b_chats = await db.get_banned()
         temp.BANNED_USERS = b_users
         temp.BANNED_CHATS = b_chats
         await super().start()
         await Media.ensure_indexes()
-        btn = [
-        [
-            InlineKeyboardButton('⚡️ ℂ𝕀ℕ𝔼𝕄𝔸 ℍ𝕌𝔹 ⚡️', url=f'https://t.me/cinemahub02')
-        ]
-        ]
+        usr_bot_me = await self.get_me()
+        self.set_parse_mode("html")
+        self.USER, self.USER_ID = await User().start()
         await self.USER.send_message(
             chat_id=-1001308633613,
             text="---------------------"
         )
-        me = await self.get_me()
-        temp.ME = me.id
-        temp.U_NAME = me.username
-        temp.B_NAME = me.first_name
-        self.username = '@' + me.username
-        logging.info(f"{me.first_name} with Pyrogram v{__version__} (Layer {layer}) started.")
-        logging.info(LOG_STR)
 
     async def stop(self, *args):
         await super().stop()
-        await self.send_message(
-            chat_id=-1001308633613,
-            text="🧭🧭 <b>GROUP CLOSED</b> 🧭🧭\n\n✅ Requests are allowed, Let's start.\n\n🌄 Good morning."
-        )
-        logging.info("Bot stopped. Bye.")
-
-
-app = Bot()
-app.run()
