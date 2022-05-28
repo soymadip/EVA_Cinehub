2cf18eec575f2835c7b3a35589b801b48ef19b06
soumadip <soumadip377@gmail.com>
2022-05-28 19:47:41 +0530

    Create auto_edit.py

---
+++ plugins/auto_edit.py
@@ -0,0 +1,19 @@
+
+import asyncio
+from pyrogram import Client, filters
+
+
+
+
+@Client.on_message(filters.channel & (filters.document | filters.video | filters.audio ) & ~filters.edited, group=-1)
+async def editing(bot, message):
+     caption_text = message.file.file_name
+     await bot.edit_message_caption(
+                 chat_id = message.chat.id,
+                 message_id = message.message_id,
+                 caption = caption_text, 
+                 parse_mode = "markdown"
+             ) 
+              
+                   
+      
