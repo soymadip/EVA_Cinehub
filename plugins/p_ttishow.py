522513d83f534cfc4097a7d5afa829e4b82e9e36
soumadip <soumadip377@gmail.com>
2022-05-31 16:58:37 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -55,7 +55,15 @@ async def save_group(bot, message):
                         await (temp.MELCOW['welcome'])
                     except:
                         pass
-                temp.MELCOW['welcome'] = await message.reply(f"Hey {u.mention} {MELCOW_NEW_TEXT}", parse_mode='html')
+                wel_text = MELCOW_NEW_TEXT.format(
+                                    first = message.from_user.first_name,
+                                    username = None if not message.from_user.username else '@' + message.from_user.username,
+                                    mention = message.from_user.mention,
+                                    id = message.from_user.id,
+                                    dcid = message.from_user.dc_id,
+                                    chatname = message.chat.title
+                                )
+                temp.MELCOW['welcome'] = await message.reply(f"{wel_text}", parse_mode='html')
                         
 
 
