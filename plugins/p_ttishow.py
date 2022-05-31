ea8b40ffab8b46226d6208c444605b81302ab645
soumadip <soumadip377@gmail.com>
2022-05-31 18:04:52 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -58,7 +58,7 @@ async def save_group(bot, message):
                         pass
                 MELOW_TEXT = MELCOW_NEW_TEXT.format(
                                     mention = u.mention,
-                                    chat = u..chat.title
+                                    chat = u.chat.title
                                 )
                 temp.MELCOW['welcome'] = await message.reply(f"{MELOW_TEXT}")
 
