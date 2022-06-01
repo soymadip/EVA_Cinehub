0f67bd1e13c153d86f910937a47232110b12bc78
soumadip <soumadip377@gmail.com>
2022-06-01 07:00:25 +0530

    Update nightmode.py

--- plugins/SD_plugins/nightmode.py
+++ plugins/SD_plugins/nightmode.py
@@ -33,6 +33,6 @@ async def group_open():
 
 
 scheduler = AsyncIOScheduler(timezone=TIMEZONE)
-scheduler.add_job(group_close, trigger="cron", hour=7, minute=0)
+scheduler.add_job(group_close, trigger="cron", hour=7, minute=1)
 scheduler.add_job(group_open, trigger="cron", hour=8, minute=1)
 scheduler.start()
